d9d9ca418fed85a778504f7ea55fc4ca4c914964
isro24 <alamat_email_anda@example.com>
2024-10-17 12:48:57 +0700

    part2

---
+++ routes/todo.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+let todos = [
+    {
+        id: 1,
+        task: "Belajar Node.js",
+        completed: false
+    },
+    {   
+        id: 2,
+        task: "Membuat API",
+        completed: false
+    }
+];
+
+router.get('/', (req, res) => {res.json(todos);});
+
+router.post('/', (req, res) => {
+    const newTodo = {
+        id: todos.length + 1,
+        task: req.body.task,
+        completed: false
+    };
+    todos.push(newTodo);
+    req.statusCode(201).json(newTodo);
+});
+
+module.exports = router;
